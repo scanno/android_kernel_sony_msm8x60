3af9d7630483aa214afc956ae55317c83526a34d
AdrianDC <radian.dc@gmail.com>
2015-06-30 00:33:43 +0200

    msm: SONY PM8921 Power Manager for SONY Huashan
    Change-Id: I28376b2ca0c88d55f43d15831c8fe904cf301598
    
    Conflicts:
    	arch/arm/configs/cm_viskan_huashan_defconfig
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/Makefile
    	arch/arm/mach-msm/devices-8960.c
    	arch/arm/mach-msm/pm-8x60.c
    	drivers/power/pm8921-charger.c

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -1744,7 +1744,7 @@ struct platform_device msm8960_cpu_slp_status = {
 
 static struct msm_watchdog_pdata msm_watchdog_pdata = {
 	.pet_time = 15000,
-	.bark_time = 25000,
+	.bark_time = 45000,
 	.has_secure = true,
 	.base = MSM_TMR0_BASE + WDT0_OFFSET,
 };
